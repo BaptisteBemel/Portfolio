adb5ef110c0a903e0a0cecae5f90bc38a9b6e4c3
BaptisteBemel <baptiste@bemelmans.eu>
2022-05-29 21:42:40 +0200

    suite presentation

--- src/Components/intro/Intro.js
+++ src/Components/intro/Intro.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./intro.css"
+import Doge from "../../img/Doge.png"
 
 function Intro() {
   return (
@@ -7,7 +8,6 @@ function Intro() {
       <div className='left'>
         <div className='l-wrapper'>
           <h2 className='name'>Baptiste Bemelmans</h2>
-          <h2 className='pres-moi'>20 ans belge, etudiant 3ème ephec voici mon protfolio blabla</h2>
           <h2 className='acti-title'>Acti valorisées</h2>
           <div className='acti'>
             <div className='acti-wrapper'>
@@ -17,10 +17,16 @@ function Intro() {
               <div className='acti-item'>acti4</div>
               <div className='acti-item'>acti5</div>
             </div>
-          </div>          
+          </div>  
+          <div className='pres-moi'>
+            20 ans belge, etudiant 3ème ephec voici mon protfolio blabla
+          </div>        
         </div>
       </div>
-      <div className='right'></div>
+      <div className='right'>
+        <div className='bg'></div>
+        <img src={Doge} alt='' className='image' />
+      </div>
     </div>
   )
 }
